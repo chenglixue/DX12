2e87babda51b8e4dba72dc187420c60055faf1f0
Elysia <86976832+chenglixue@users.noreply.github.com>
2023-02-05 16:20:29 +0800

    Delete Main.cpp

--- Main.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "D3D12HelloTriangle.h"
-
-_Use_decl_annotations_
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
-{
-	D3D12HelloTriangle window(1920, 1080, L"登上山巅，仰望之处更是群山");
-	return Win32Application::Run(&window, hInstance, nCmdShow);
-}
